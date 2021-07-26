ab3d25b0af99a3ee9612ee8e5db29de767cdeca0
mac rent <macrent@macs-air-2.home>
2021-07-26 19:53:26 +1000

    last commit on MAC

---
+++ Kathryn Lugton/comp-sci/search-algorithms/searches.js
@@ -0,0 +1,49 @@
+function binarySearch(haystack, needle){
+  let start = 0;
+  let end = haystack.length -1
+  let midpoint = Math.floor( (start + end) / 2);
+
+  //not needle is not found AND still have haystack to consider
+  while (haystack[midpoint] !== needle && start < end) {
+    if(needle < haystack[midpoint]) {
+      end = midpoint -1;
+    }else {
+      start = midpoint + 1;
+    }
+    midpoint = Math.floor( (start + end ) / 2);
+  }
+  //
+  // //ternary:
+  // return (haystack[midpoint] === needle) ? midpoint : -1;
+  if(haystack[midpoint] === needle) {
+    return midpoint; //index of the needle in the haystack
+  } else {
+    return -1;
+  }
+}
+
+function recursiveBinarySearch(haystack,
+                                needle,
+                                start=0,
+                                end=haystack.length-1,
+                                midpoint=Math.floor((start + end ) /2 )){
+  if(haystack[midpoint] === needle) {
+    return midpoint; //base case - success
+  }
+
+  if( start >= end ){
+    return -1; //base case - failure
+  }
+
+  if(needle > haystack[midpoint]) {
+    start = midpoint + 1;
+  }else {
+    end = midpoint -1;
+  }
+  return recursiveBinarySearch(haystack, needle, start, end);
+}
+
+module.exports = {
+    binarySearch,
+    recursiveBinarySearch
+}
